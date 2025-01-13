e03197424354ca3bb8afa9083a83ee23a883ea64
fahmi416 <fahmifikri9393@gmail.com>
2025-01-14 03:26:57 +0700

    commit kedua

---
+++ main.py
@@ -0,0 +1,13 @@
+import time
+start_time = time.time()
+
+print("hello world")
+
+print("hello")
+#comment
+""""comment multiline
+aku ganteng"""
+a = 100
+print(a)
+
+print (time.time()-start_time, "detik")
